9cc5b9c69655b1a2f4967689f18f8be8a68ee758
Heikki Orsila <heikki.orsila@iki.fi>
2005-12-07 19:04:10 +0000

    *** empty log message ***

--- tests/check.sh
+++ tests/check.sh
@@ -1,3 +1,11 @@
 prog="../amigadepacker"
 
-$prog -c < sqsh1 |md5sum
+name="Test 1"
+file="sqsh1"
+md5="`$prog -c < $file |md5sum |cut -d ' ' -f1`"
+if test "$md5" != "b3659301b360a83bd737ef80201ddcaf" ; then
+    echo $name error
+    exit -1
+fi
+
+exit 0
